1d58629ce641dbf0f0eef14f5e88d30697163732
gabyzg <100975074+gabyzg@users.noreply.github.com>
2023-09-25 18:34:29 +0000

    ⭐ feature shopping cart agrega icono de titulo

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,19 @@
 <script setup>
 
+import { ref } from 'vue';
+const header = ref('Mi Carrito de Compras')
+
 </script>
 
 <template>
-  <h1> my shopping cart</h1>
+  <h1><i class="material-icons shopping-cart-icon">local_mall</i> {{ header }}</h1>
 </template>
 
 <style scoped>
+
+.shopping-cart-icon{
+  font-size: 2rem;
+
+}
 
 </style>
