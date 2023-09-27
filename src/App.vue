aaf656a1c8357264da96082a9de533f2d8fe42e3
gabyzg <100975074+gabyzg@users.noreply.github.com>
2023-09-27 17:57:39 +0000

    listas

--- src/App.vue
+++ src/App.vue
@@ -3,19 +3,19 @@
 import { ref } from 'vue';
 const header = ref('Mi Carrito de Compras');
 
-const items = ref({
-  "item1": { id: 1, label: '3 chocolates' },
-  "item2": { id: 2, label: '1 jugo' },
-  "item3": { id: 3, label: '3 galletas' },
-});
+const items = ref([
+  {id: 1, label: '3 Chocolates'},
+  {id: 2, label: '1 jugo'},
+  {id: 3, label: '4 galletas'}
+]);
 </script>
 
 <template>
-  <h1>{{ header }}</h1>
+  <h1> <i class="material-icons shopping-cart-icon">local_mall</i> {{ header }}</h1>
   <ul>
-    <li v-for="({ id, label }, index) in items" v-bind:key="id">
-			🔹 -{{ index }}- {{ label }}
-		</li>
+    <li v-for="{ id, label } in items" v-bind:key="id">
+      🔹 {{ label }}
+    </li>
   </ul>
   
 
